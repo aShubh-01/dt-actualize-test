429d0ff8a988aa7a418f53cd14291d5fb6157d75
Deepansu Mor <deepansumor@gmail.com>
2025-05-09 18:10:15 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-/** @type {import("next").NextConfig} */
-module.exports = {
-}
+// next.config.js
+const nextConfig = {
+    basePath: '/dt-actualize',
+    assetPrefix: '/dt-actualize',
+  };
+  
+  module.exports = nextConfig;
+  
