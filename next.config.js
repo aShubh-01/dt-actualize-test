58a36ddd6c865860d00c552f1703a4d9152d355f
Megha <karmakarmegha01@gmail.com>
2025-06-20 18:17:59 +0530

    changes in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 const nextConfig = {
   images: {
-    domains: ['storage.googleapis.com'],
+    domains: ['storage.googleapis.com', 'ui-avatars.com', 'picsum.photos'],
   },
     assetPrefix: '/dt-actualize',
   };
